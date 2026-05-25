b6e4b62db630d6068773810a38fdfb430bb4ec05
Lucas Coelho <lucas.coelho@sptech.school>
2026-05-25 20:39:36 -0300

    Feat: Foi feita a rota para os dados dos jogadores.

--- web-data-viz/app.js
+++ web-data-viz/app.js
@@ -27,7 +27,6 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use(cors());
 
-// app.use("/", indexRouter);
 app.use("/usuarios", usuarioRouter);
 app.use("/tentativas", tentativaRouter);
 app.use("/jogadores", jogadoresRouter);
