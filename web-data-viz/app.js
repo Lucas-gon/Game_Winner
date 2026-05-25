a961be9a3c08ade870ddab0fd56ab3fb913681e0
Lucas Coelho <lucas.coelho@sptech.school>
2026-05-25 18:01:29 -0300

    Feat: Foi criada a rota para os jogadores.

--- web-data-viz/app.js
+++ web-data-viz/app.js
@@ -18,6 +18,7 @@ var app = express();
 // var indexRouter = require("./src/routes/index");
 var usuarioRouter = require("./src/routes/usuarios");
 var tentativaRouter = require("./src/routes/tentativas");
+var jogadoresRouter = require("./src/routes/jogadores");
 
 
 app.use(express.json());
@@ -29,6 +30,7 @@ app.use(cors());
 // app.use("/", indexRouter);
 app.use("/usuarios", usuarioRouter);
 app.use("/tentativas", tentativaRouter);
+app.use("/jogadores", jogadoresRouter);
 
 app.listen(PORTA_APP, function () {
     console.log(`
